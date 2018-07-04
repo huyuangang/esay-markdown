bbf5c43196bf4bfa5762a5345d0c82d1170deb52
huyuangang <huyuangang@didichuxing.com>
2018-07-04 21:34:16 +0800

    修改gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 
 gulp.task('default', function() {
-  gulp.src('src/*.js')
-  .pipe(concat('parse.js'))
+  gulp.src(['src/index.js', 'src/inline.js', 'src/block.js'])
+  .pipe(concat('easymarkdown.js'))
   .pipe(gulp.dest('dist/'))
 })
